dd2dfa68b55c992181128f47e506c967c6030d53
Menno Guldemond <mennoguldemond@live.nl>
2023-12-30 19:30:02 +0100

    remove comment

--- src/interaction-handler.js
+++ src/interaction-handler.js
@@ -4,7 +4,6 @@ const logService = require('./services/log.service')
 const messageBuilder = require('./utils/message-builder')
 
 async function handle(interaction) {
-  //   const user = await userService.getById(interaction.user.id)
   // The customId property is a string formatted as "originSceneId.newSceneId.optionNumber.timestamp"
   const oldSceneId = interaction.customId.split('.')[0]
   const nextSceneId = interaction.customId.split('.')[1]
